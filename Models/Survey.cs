1f8f8d2e5816d6afa19d90ea2bed1219ae9b81dc
ptr47 <zdz437@protonmail.com>
2024-05-20 01:57:40 +0200

    small style changes and starting survey creation

--- Models/Survey.cs
+++ Models/Survey.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models
 {
     public class Survey
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(99)]
         public string Title { get; set; }
         public IEnumerable<Question> Questions { get; set; }
         public IEnumerable<Answer> Answers { get; set; }
