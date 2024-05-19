7d4416aede70210fff5d6157a00f3522a41b89bc
ptr47 <zdz437@protonmail.com>
2024-05-20 00:30:31 +0200

    adding (maybe?) blazor bootstrap

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddAuthentication(options =>
     {
